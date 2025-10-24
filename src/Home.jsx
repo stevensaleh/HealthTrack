7971deacb79f114f75b4a90f3694d2e676bffa89
Steven Saleh <sfsaleh@umich.edu>
2025-10-24 17:36:29 -0400

    Added image

--- src/Home.jsx
+++ src/Home.jsx
@@ -21,9 +21,21 @@ export default function Home() {
       >
         Sign In
       </button>
-      <h1 style={{ marginBottom: '1rem', fontFamily: "'Playfair Display'"  }}>Step Into Intelligent</h1>
-      <h1 style={{ marginTop: 0, marginBottom: '2rem', fontFamily: "'Playfair Display'"  }}>Health Tracking</h1>
-      <h3 style={{ marginTop: 0 , fontFamily: "'Playfair Display'"  }}>Your journey to a healthier you starts here</h3>
+      <h1 style={{ marginBottom: '15px', fontFamily: "'Playfair Display'"  }}>Step Into Intelligent</h1>
+      <h1 style={{ marginTop: 0, marginBottom: '30px', fontFamily: "'Playfair Display'"  }}>Health Tracking</h1>
+      <h3 style={{ marginTop: 0, fontFamily: "'Playfair Display'"  }}>Your journey to a healthier you starts here</h3>
+      <img
+        src="https://hips.hearstapps.com/hmg-prod/images/gettyimages-1166696259.jpg?crop=1.00xw:0.753xh;0,0.127xh&resize=1200:*"
+        style={{
+          marginTop: '25px',
+          width: '100%',
+          maxWidth: '1500px',
+          height: '600px',
+          objectFit: 'cover',
+          borderRadius: '20px',
+          boxShadow: '0 8px 24px rgba(0,0,0,0.15)'
+        }}
+      />
     </div>
   );
 }
