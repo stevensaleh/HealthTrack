dc3222970ca2fdc50a72ab49a80f1738091051c3
Steven Saleh <sfsaleh@umich.edu>
2025-10-24 16:29:56 -0400

    Removed buttons, added sign in button

--- src/Home.jsx
+++ src/Home.jsx
@@ -12,24 +12,18 @@ export default function Home() {
         overflowX: "auto",
       }}
     >
+      <button
+        style={{
+          position: "fixed",
+          top: "20px",
+          right: "100px",
+        }}
+      >
+        Sign In
+      </button>
       <h1 style={{ marginBottom: '0.25rem' }}>Step Into Intelligent</h1>
       <h1 style={{ marginTop: 0 }}>Health Tracking</h1>
       <h3>Your journey to a healthier you starts here</h3>
-      
-      <div
-        style={{
-          marginTop: "200px",
-          display: "flex",
-          justifyContent: "center",
-          gap: "50px",
-          fontSize: "1.2rem",
-        }}
-      >
-        <button>Button 1</button>
-        <button>Button 2</button>
-        <button>Button 3</button>
-      </div>
-
     </div>
   );
 }
